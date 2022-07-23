a713b45ac16250078589286a549f4266da37f9c3
iammukulsaini <101089211+iammukulsaini@users.noreply.github.com>
2022-07-23 15:26:29 +0530

    weather-app

---
+++ src/components/HeaderButtons.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+function HeaderButtons({ setQuery }) {
+  const cities = [
+    {
+      id: 1,
+      title: "Delhi",
+    },
+    {
+      id: 2,
+      title: "Mumbai",
+    },
+    {
+      id: 3,
+      title: "Kolkata",
+    },
+    {
+      id: 4,
+      title: "Chennai",
+    },
+    {
+      id: 5,
+      title: "Ahmedabad",
+    },
+  ];
+
+  return (
+    <div className="flex items-center justify-around my-6">
+      {cities.map((city) => (
+        <button
+          key={city.id}
+          className="text-white text-lg font-medium"
+          onClick={() => setQuery({ q: city.title })}
+        >
+          {city.title}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default HeaderButtons;
